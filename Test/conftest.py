4c792a38b499e3b61d8a0465f52b8427309fcda8
gonzamol17 <gmolina083@alumnos.iua.edu.ar>
2025-02-13 16:43:12 +0100

    actualizacion repo para generacion reporte

--- Test/conftest.py
+++ Test/conftest.py
@@ -36,7 +36,8 @@ def test_setup(request):
     driver.quit()
 
 
-@pytest.mark.hookwrapper
+#@pytest.mark.hookwrapper
+@pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item):
     """
         Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
@@ -48,6 +49,7 @@ def pytest_runtest_makereport(item):
     extra = getattr(report, 'extra', [])
 
     if report.when == 'call' or report.when == "setup":
+        extra.append(pytest_html.extras.url("http://opencart.abstracta.us"))
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
             file_name = report.nodeid.replace("::", "_") + ".png"
@@ -56,10 +58,10 @@ def pytest_runtest_makereport(item):
                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
                 extra.append(pytest_html.extras.html(html))
-        report.extra = extra
+        report.extras = extra
 
 
 def _capture_screenshot(name):
     #driver.get_screenshot_as_file(name)
-    driver.get_screenshot_as_file("..\\Test\\Screenshots\\"+name)
+    driver.get_screenshot_as_file("..\\Test\\Reports\\"+name)
 
